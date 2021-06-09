35a2d886504868ee2755d3cf5072882ac667799f
jromerooo2 <75763058+jromerooo2@users.noreply.github.com>
2021-06-09 17:42:32 -0600

    initial commit

--- js/main.js
+++ js/main.js
@@ -124,14 +124,9 @@
             e.preventDefault();
             //alert(op);
 
-          
-            $(form).addClass('calculator');
-
-            setTimeout(()=>{
-
                 txtCalc.html(rtrn); 
-                $(form).removeClass('calculator');  
-            }, 1800);
+                 
+           
 
         });
 
